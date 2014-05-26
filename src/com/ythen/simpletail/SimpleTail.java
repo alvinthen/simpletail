9ced00e41519a126b355347b5268e5f2c1419267
Yaobin <alvinthen890703@gmail.com>
2014-05-27 00:55:06 +0800

    Implemented main entry point

--- src/com/ythen/simpletail/SimpleTail.java
+++ src/com/ythen/simpletail/SimpleTail.java
@@ -3,14 +3,130 @@ package com.ythen.simpletail;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.io.RandomAccessFile;
 import java.io.Writer;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 
 public class SimpleTail {
 	final static long DEFAULT_LINES = 10;
 
+	@SuppressWarnings("static-access")
 	public static void main(String[] args) {
-		
+		// Set up CLI
+		Options options = new Options();
+
+		options.addOption(OptionBuilder
+				.withLongOpt("lines")
+				.withDescription(
+						"output the last K lines, instead of the last 10; or use -n +K to output lines starting with the Kth")
+				.hasArg().withArgName("K").create('n'));
+
+		options.addOption(OptionBuilder
+				.withLongOpt("bytes")
+				.withDescription(
+						"output the last K bytes; alternatively, use -c +K to output lines starting with the Kth of each file")
+				.hasArg().withArgName("K").create('c'));
+
+		options.addOption(OptionBuilder.withLongOpt("help")
+				.withDescription("display this help and exit").create());
+
+		// Parse the command line arguments
+		CommandLineParser parser = new GnuParser();
+		PrintWriter pw = new PrintWriter(System.out);
+
+		long value = 0;
+		boolean tail, tailFromLine, tailFromByte, tailBytes;
+		tail = tailFromLine = tailFromByte = tailBytes = false;
+
+		try {
+			CommandLine cl = parser.parse(options, args);
+
+			if (cl.hasOption("help")) {
+				printUsage(options);
+				return;
+			}
+
+			String[] files = cl.getArgs();
+			if (files.length == 0) {
+				System.err.println("No files specified.");
+				printUsage(options);
+				return;
+			}
+
+			if (cl.hasOption("n")) {
+				String op = cl.getOptionValue("n");
+				if (op.contains("+")) {
+					tailFromLine = true;
+					value = Integer.parseInt(op.substring(1));
+				} else if (Integer.parseInt(op) < 0) {
+					System.err.println("Invalid parameter.");
+					printUsage(options);
+					return;
+				} else {
+					tail = true;
+					value = Integer.parseInt(op);
+				}
+			}
+
+			if (cl.hasOption("c")) {
+				String op = cl.getOptionValue("c");
+				if (op.contains("+")) {
+					tailFromByte = true;
+					value = Integer.parseInt(op.substring(1));
+				} else if (Integer.parseInt(op) < 0) {
+					System.err.println("Invalid parameter.");
+					printUsage(options);
+					return;
+				} else {
+					tailBytes = true;
+					value = Integer.parseInt(op);
+				}
+			}
+
+			for (int i = 0; i < files.length; i++) {
+				System.out.println("==> " + files[i] + " <==");
+
+				// Expand home directory symbol
+				if (files[i].contains("~"))
+					files[i] = System.getProperty("user.home")
+							+ files[i].substring(1);
+
+				if (tail)
+					new SimpleTail().tail(files[i], pw, value);
+				else if (tailFromLine)
+					new SimpleTail().tailFromLine(files[i], pw, value);
+				else if (tailBytes)
+					new SimpleTail().tailBytes(files[i], pw, value);
+				else if (tailFromByte)
+					new SimpleTail().tailFromByte(files[i], pw, value);
+				else
+					new SimpleTail().tail(files[i], pw);
+
+				pw.flush();
+			}
+		} catch (ParseException ex) {
+			System.err.println(ex.getMessage());
+			printUsage(options);
+		} catch (NumberFormatException ex) {
+			System.err.println("Invalid parameter " + ex.getMessage());
+			printUsage(options);
+		} catch (Exception ex) {
+			System.err.println(ex.getMessage());
+			printUsage(options);
+		}
+	}
+
+	private static void printUsage(Options opt) {
+		HelpFormatter hf = new HelpFormatter();
+		hf.printHelp("SimpleTail [OPTIONS]... [FILES]...", opt);
 	}
 
 	/**
@@ -174,7 +290,7 @@ public class SimpleTail {
 	public void tailFromByte(File file, Writer writer, long b)
 			throws FileNotFoundException, IOException {
 		RandomAccessFile raf = new RandomAccessFile(file, "r");
-		
+
 		if (b == 0 || b >= raf.length())
 			return;
 
